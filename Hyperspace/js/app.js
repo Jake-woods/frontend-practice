b0b4af24194467d41806b8cc9cace48bfb664970
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-30 09:53:45 +0000

    Nav behaviourand design

--- Hyperspace/js/app.js
+++ Hyperspace/js/app.js
@@ -1,15 +1,18 @@
 const navLinks = $('.nav-link');
 
 $(window).scroll(() => {
+   // Gets scroll location
    const scrollLocation = $(this).scrollTop();
 
    navLinks.each(function () {
+      // How far from the top
       const sectionOffSet = $(this.hash).offset().top;
 
       // If in view
       if (sectionOffSet <= scrollLocation) {
          $(this).parent().addClass('active');
-         $(this).parent().siblings().removeClass('active')
+         $(this).parent().siblings().removeClass('active');
       }
-   })
+   });
+
 })
